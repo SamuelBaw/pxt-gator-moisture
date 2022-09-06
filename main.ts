5c1122949c6728f80a8e9718859688c39ca7e762
SamuelBaw <104888073+SamuelBaw@users.noreply.github.com>
2022-09-06 10:40:19 +0200

    Aktualisiere pxt.json, main.ts, main.blocks

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-// Gib deinen Code hier ein
+basic.showNumber(7)
+basic.pause(2000)
+basic.forever(function () {
+    basic.showNumber(99)
+    basic.pause(500)
+    basic.showNumber(gatorMoisture.moisture(AnalogPin.P0, gatorMoistureType.adcVal))
+    basic.pause(500)
+})
